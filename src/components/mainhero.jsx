db3dcd57abae2854da0c4ab55a2f11eefbe75c94
unknown <youngscientist.iysa@gmail.com>
2024-06-10 15:05:50 +0700

    third commit

--- src/components/mainhero.jsx
+++ src/components/mainhero.jsx
@@ -3,20 +3,14 @@ function MainHero() {
     <>
       <section className="mainhero-section">
         <div className="mainhero-wrapper">
-          <h1>Welcome to INTERNATIONAL SCIENCE OLYMPIAD</h1>
+          <h1>WELCOME TO <span>INTERNATIONAL SCIENCE OLYMPIAD</span></h1>
           <div className="content row ">
             <div className="col-lg-6">
               <a href=""></a>
             </div>
             <div className="col-lg-6 right-content">
               <p className="">
-                It's a stage for innovators, researchers, and young thinkers
-                from around the globe, including students from elementary school
-                to university. Compete in a collaborative environment across
-                diverse categories such as Mathematics, Physics, Energy and
-                Engineering, Life Sciences, Environment, Education, Technology,
-                and Social Science. Showcase your ideas, gain recognition, and
-                contribute to the future of science. Register now!
+                It's a platform for innovators, researchers, and young thinkers from around the globe, with a focus on university students. Compete in a collaborative environment across diverse categories such as Mathematics, Physics, Energy and Engineering, Life Sciences, Environment, Education, Technology, and Social Science. Showcase your ideas, gain recognition, and contribute to the future of science. Register now!
               </p>
               <a className="btn btn-action" href="https://registrasi-isif-2024.vercel.app/" target="_blank">
                 Link Registration
