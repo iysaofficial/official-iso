c9565802c2d274f82db0624b064735bfc5aeb0aa
unknown <youngscientist.iysa@gmail.com>
2024-06-10 14:05:37 +0700

    first commit

---
+++ src/components/about.jsx
@@ -0,0 +1,43 @@
+function About() {
+  return (
+    <>
+      <section id="about" className="about-section ">
+        <div className="container row mx-auto">
+          <h1 className="text-center mb-3">About ISO</h1>
+          <div className="content-img col-lg-6 col-12 text-center">
+            <img src="./assets/logo/ISO.png" className="img-fluid" alt="" />
+          </div>
+          <div className="content-text col-lg-6 col-12">
+            <p className="mb-2">
+              By looking at the current development and knowledge of children
+              about science, inventions and innovations, especially in the field
+              of science, it requires us to know the extent of students'
+              insights, knowledge and abilities in applying their scientific
+              knowledge.
+            </p>
+            <p className="mb-2">
+              Therefore, the Indonesian Young Scientist Association (IYSA) will
+              hold an international level Invention competition called
+              "International Science Olympiad (ISO)" as the right learning
+              platform to nurture our talented young inventors. active, creative
+              and innovative.
+            </p>
+            <p className="mb-2">
+              The Indonesian Young Scientist Association (IYSA) is an
+              institution engaged in developing the potential, talents and
+              creativity of Indonesian students in non- scientific competitions
+              and competitions at home and abroad. Therefore, IYSA stands as a
+              form of our deep concern in providing opportunities for Indonesian
+              students in particular, to bring out the potential and skills
+              needed as well as a spirit of competition that builds within them.
+              We do this to create life improvements. Because we believe that
+              every student has potential that can be developed.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default About;
---
+++ src/components/mainhero.jsx
@@ -0,0 +1,39 @@
+function MainHero() {
+  return (
+    <>
+      <section className="mainhero-section">
+        <div className="mainhero-wrapper">
+          <h1>Welcome to INTERNATIONAL SCIENCE OLYMPIAD</h1>
+          <div className="content row ">
+            <div className="col-lg-6">
+              <a href=""></a>
+            </div>
+            <div className="col-lg-6 right-content">
+              <p className="">
+                It's a stage for innovators, researchers, and young thinkers
+                from around the globe, including students from elementary school
+                to university. Compete in a collaborative environment across
+                diverse categories such as Mathematics, Physics, Energy and
+                Engineering, Life Sciences, Environment, Education, Technology,
+                and Social Science. Showcase your ideas, gain recognition, and
+                contribute to the future of science. Register now!
+              </p>
+              <a className="btn btn-action" href="https://registrasi-isif-2024.vercel.app/" target="_blank">
+                Link Registration
+              </a>
+              <a
+                className="btn btn-action"
+                href="https://drive.google.com/file/d/1fHAV9_CycmJoHqPbEK3rD2I1kminYu6L/view?usp=sharing"
+                target="_blank"
+              >
+                Guide Book
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default MainHero;
